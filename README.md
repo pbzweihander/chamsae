75e253067369c3894ef073f8b9ed8424bea06f89
pbzweihander <pbzweihander@gmail.com>
2023-08-06 20:01:57 +0900

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,19 @@
+# chamsae
+
+**chamsae** is a lightweight ActivityPub server for a single user.
+chamsae is for a single user who wants to join the fediverse, but not want to run a massive software.
+[_chamsae_ means an Eurasian tree sparrow.](https://en.wikipedia.org/wiki/Eurasian_tree_sparrow)
+
+## Features
+
+- Mastodon or Misskey like microblogging service
+- Misskey like emoji reactions
+
+## Usage
+
+TODO
+
+## License
+
+_chamsae_ is licensed under the terms of [AGPL v3.0](https://www.gnu.org/licenses/agpl-3.0.html).
+See [LICENSE file](./LICENSE) for details.
---
+++ migration/src/m20230806_104639_initial.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Post::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Post::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Post::Title).string().not_null())
+                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(Iden)]
+enum Post {
+    Table,
+    Id,
+    Title,
+    Text,
+}
