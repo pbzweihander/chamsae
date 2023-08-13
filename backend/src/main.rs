6851528f7e391a943d2370132573b3eeedc52b85
pbzweihander <pbzweihander@gmail.com>
2023-08-14 04:38:36 +0900

    Suppress sqlx logs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -52,7 +52,8 @@ async fn main() -> anyhow::Result<()> {
 
     tracing_subscriber::registry()
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "info,sqlx=warn".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .with(tracing_error::ErrorLayer::default())
