9eecb9811df4073288fc1b41eb90b98b3f4b444c
Wonwoo Choi <chwo9843@gmail.com>
2023-09-23 22:54:47 +0900

    frontend: Update dependencies (#25)
    * frontend: Update dependencies
    
    * frontend: Force dynamic rendering
    
    * frontend: More dynamic pages

--- frontend/src/app/(initialized)/layout.tsx
+++ frontend/src/app/(initialized)/layout.tsx
@@ -1,5 +1,7 @@
 import getSetting from "@/lib/api/getSetting";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: {
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,8 @@ import getAccessKey from "@/lib/api/getAccessKey";
 import getSetting from "@/lib/api/getSetting";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function Main() {
   await getSetting();
   const accessKey = await getAccessKey();
