28f6539b646a2573f065e5865fc329b7731a803e
pbzweihander <pbzweihander@protonmail.com>
2024-05-14 02:04:19 +0900

    frontend: Use different index for dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    {
+      name: "index-html-env",
+      transformIndexHtml: {
+        order: "pre",
+        async handler() {
+          if (mode !== "production") {
+            return await fs.readFile("index.dev.html", "utf8");
+          }
+        },
+      },
+    },
+  ],
+  server: {
+    proxy: {
+      "/api": "http://127.0.0.1:3000",
+    },
+  },
+}));
